0c1f53667b4c7e393b5ebfabfa6043954cd5605c
Zuhaib <xuhaib_ahmad@yahoo.com>
2020-04-18 16:30:53 +0800

    Add fragment with theme

--- app/src/main/java/com/zuhaibahmad/netflixgriddemo/ui/main/MainFragment.kt
+++ app/src/main/java/com/zuhaibahmad/netflixgriddemo/ui/main/MainFragment.kt
@@ -1,11 +1,10 @@
 package com.zuhaibahmad.netflixgriddemo.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.zuhaibahmad.netflixgriddemo.R
 
 class MainFragment : Fragment() {
@@ -14,17 +13,14 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.main_fragment, container, false)
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
     }
 
 }
--- app/src/main/java/com/zuhaibahmad/netflixgriddemo/ui/main/MainViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.zuhaibahmad.netflixgriddemo.ui.main
-
-import androidx.lifecycle.ViewModel
-
-class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
